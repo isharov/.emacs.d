773e8e94f9113f12ec799247b971d3d7c47bcbd7
Ivan Sharov <charoff@gmail.com>
2014-12-01 13:53:25 +0500

    Recent files with nondirectory

--- init.el
+++ init.el
@@ -447,6 +447,11 @@
 
 (defun recentf/ido-find-file ()
   (interactive)
-  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
-    (when file
-      (find-file file))))
+  (let* ((file-assoc-list
+          (mapcar (lambda (x)
+                    (cons (file-name-nondirectory x) x))
+                  recentf-list))
+         (filename-list (remove-duplicates (mapcar #'car file-assoc-list) :test #'string=))
+         (filename (ido-completing-read "Choose recent file: " filename-list nil t)))
+    (when filename
+      (find-file (cdr (assoc filename file-assoc-list))))))
