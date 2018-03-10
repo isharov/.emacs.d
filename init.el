64cf553ff7c3af25ccdf887b34bdba44552bff6b
Ivan Sharov <charoff@gmail.com>
2018-03-10 13:45:58 +0500

    Switch to stable melpa.

--- init.el
+++ init.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 
 (setq custom-file "~/.emacs.d/custom.el")
