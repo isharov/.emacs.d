c3b8ab621f2aedafd40841807c7f26ec25bdf605
Ivan Sharov <charoff@gmail.com>
2020-04-13 12:09:00 +0300

    Using kubel package

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (multiple-cursors expand-region smart-forward move-text buffer-move avy helm helm-swoop helm-ag company magit diff-hl helm-ls-git docker docker-tramp dockerfile-mode flycheck lsp-mode company-lsp lsp-ui go-mode rjsx-mode web-mode yaml-mode restclient spacemacs-theme dsvn markdown-mode dash-functional))))
+    (multiple-cursors expand-region smart-forward move-text buffer-move avy helm helm-swoop helm-ag company magit diff-hl helm-ls-git docker docker-tramp dockerfile-mode kubel flycheck lsp-mode company-lsp lsp-ui go-mode rjsx-mode web-mode yaml-mode restclient spacemacs-theme dsvn markdown-mode dash-functional))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
