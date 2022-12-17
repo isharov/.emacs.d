f6fef48805122cf779c03451d33bf79e4c8a08c6
Ivan Sharov <isharov@Ivans-MacBook-Pro.local>
2022-12-17 21:05:28 +0400

    Per helm update

--- init.el
+++ init.el
@@ -91,7 +91,6 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; helm mode
-;(require 'helm-config)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-mini)
