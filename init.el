2d8af2b4941530d6988f3661af5bd7bd65c5d152
Ivan Sharov <isharov@Ivans-MacBook-Pro.local>
2022-12-20 19:16:02 +0400

    Use `macos-trash` in ventura

--- init.el
+++ init.el
@@ -359,7 +359,7 @@
   (defun system-move-file-to-trash (file)
     "Use \"trash\" to move FILE to the system trash.
      When using Homebrew, install it using \"brew install trash\"."
-    (call-process (executable-find "trash") nil 0 nil "-F" file))
+    (call-process (executable-find "trash") nil 0 nil file))
   )
 
 (provide 'init)
