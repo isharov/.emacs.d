dd8648305f02322dde03b412ff65123187c9d2e9
Ivan Sharov <isharov@Ivans-MacBook-Pro.local>
2023-08-12 11:51:29 +0300

    Initial tree-sitter support

--- init.el
+++ init.el
@@ -184,12 +184,43 @@
 ;; (global-set-key (kbd "M-'") 'insert-pair)
 (global-set-key (kbd "M-)") 'delete-pair)
 
+;; tree-sitter
+(setq treesit-language-source-alist
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (go "https://github.com/tree-sitter/tree-sitter-go")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+        (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")))
+; (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
+(setq major-mode-remap-alist
+      '((bash-mode . bash-ts-mode)
+        (js2-mode . js-ts-mode)
+        (typescript-mode . typescript-ts-mode)
+        (json-mode . json-ts-mode)
+        (css-mode . css-ts-mode)
+        (python-mode . python-ts-mode)
+        (rust-mode . rust-ts-mode)))
+; (setq python-ts-mode-hook python-mode-hook)
+(add-to-list 'load-path "~/.emacs.d/pkgs/combobulate")
+(load "combobulate.el")
+
 ;; direnv
 (direnv-mode)
 
 ;; eglot
-(add-hook 'python-mode-hook 'eglot-ensure)
-(add-hook 'rust-mode-hook 'eglot-ensure)
+(add-hook 'python-ts-mode-hook 'eglot-ensure)
+(add-hook 'rust-ts-mode-hook 'eglot-ensure)
 
 ;; spell checking
 (global-set-key (kbd "C-c s") 'isharov/toggle-flyspell)
@@ -241,7 +272,7 @@
 
 ;; python
 ;; M-x find-library RET python RET
-(add-hook 'python-mode-hook
+(add-hook 'python-ts-mode-hook
           (lambda ()
             (local-unset-key (kbd "C-c C-v"))
             (modify-syntax-entry ?_ "w") ; now '_' is not considered a word-delimiter
